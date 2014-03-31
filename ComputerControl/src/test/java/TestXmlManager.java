8d34c18c9085647b30413b6c473741c510e4d7b1
Syomin Stas <syomin.stas@gmail.com>
2014-04-01 00:34:59 +0400

    added test class

---
+++ ComputerControl/src/test/java/TestXmlManager.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * Created by spier on 4/1/14.
+ * time - 12:32 AM
+ */
+//TODO: написать тесты
+public class TestXmlManager {
+
+    @Test
+    public void testAdd() {
+
+    }
+}
