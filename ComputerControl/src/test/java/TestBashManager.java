dbb496aa5dd2fb20aeee8023ef9d5b8dc14522e2
Syomin Stas <syomin.stas@gmail.com>
2014-03-31 14:05:37 +0400

    added src

---
+++ ComputerControl/src/test/java/TestBashManager.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created by spier on 3/30/14.
+ * time - 5:16 PM
+ */
+
+//TODO: написать тесты для класса
+public class TestBashManager {
+
+    @Test
+    public void testAdd() {
+
+    }
+}
